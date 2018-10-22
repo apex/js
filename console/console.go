8cf8c19c7750a68cdab951a9cb33f080835b1361
TJ Holowaychuk <tj@vision-media.ca>
2018-10-22 14:30:56 -0700

    refactor console to use ref

--- console/console.go
+++ console/console.go
@@ -5,12 +5,15 @@ package console
 
 import "syscall/js"
 
+// console reference.
+var console = js.Global().Get("console")
+
 // Log outputs a message to the browser console.
 func Log(args ...interface{}) {
-	js.Global().Get("console").Call("log", args...)
+	console.Call("log", args...)
 }
 
 // Dir outputs an interactive list of the properties to the browser console.
 func Dir(args ...interface{}) {
-	js.Global().Get("console").Call("dir", args...)
+	console.Call("dir", args...)
 }
